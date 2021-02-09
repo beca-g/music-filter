3b2f66920533b52bebc35d88ca5b1f58dd20adf0
Beca-g <rebekahgalliano@gmail.com>
2021-02-09 16:54:18 +0000

    Corrected freqency typoo

--- spec/music_filter_spec.rb
+++ spec/music_filter_spec.rb
@@ -3,27 +3,27 @@ require "./lib/music_filter.rb"
 describe Music_Filter do
   subject(:music_filter) { described_class.new }
   describe "#filter" do
-    it "should not modify the sound wave when within the min-max frequenices" do
+    it "should not modify the sound wave when within the min-max frequencies" do
       sound_wave = music_filter.filter([40,100,1000],40,1000)
       expect(sound_wave).to eq [40,100,1000] 
     end
 
-    it "should modify the sound wave when the min frequancy is not met" do 
+    it "should modify the sound wave when the min frequency is not met" do 
       sound_wave = music_filter.filter([10,100,1000],40,1000)
       expect(sound_wave).to eq [40,100,1000] 
     end
 
-    it "should modify the sound wave when the max frequancy is not met" do 
+    it "should modify the sound wave when the max frequency is not met" do 
       sound_wave = music_filter.filter([40,100,1050],40,1000)
       expect(sound_wave).to eq [40,100,1000] 
     end
 
-    it "should modift the sound wave when the min and max frequancies are not met" do 
+    it "should modift the sound wave when the min and max frequencies are not met" do 
       sound_wave = music_filter.filter([10,100,1050],40,1000)
       expect(sound_wave).to eq [40,100,1000] 
     end
 
-    it "should raise an error if no frequancies are given" do 
+    it "should raise an error if no frequencies are given" do 
       expect { subject.filter([], 40, 100) }.to raise_error("Error: No frequencies supplied")
     end
   end
