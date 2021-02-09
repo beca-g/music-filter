e53177536db873e1b532651d6b8c44d602d9cc33
Beca-g <rebekahgalliano@gmail.com>
2021-02-09 11:41:06 +0000

    When min freq is not met the sound_wave is modified

--- spec/lib/music_filter.rb
+++ spec/lib/music_filter.rb
@@ -1,5 +1,15 @@
 class Music_Filter
+
   def filter(sound_wave, min_freq, max_freq)
-    sound_wave
+    new_sound_wave = []
+    sound_wave.each do |freq|
+      if freq < min_freq
+        new_sound_wave << min_freq
+      else
+        new_sound_wave << freq
+      end
+    end
+    new_sound_wave
   end
+
 end
--- spec/music_filter_spec.rb
+++ spec/music_filter_spec.rb
@@ -1,10 +1,15 @@
 require "./lib/music_filter.rb"
 
 describe Music_Filter do
+  subject(:music_filter) { described_class.new }
   describe "#filter" do
     it "should not modify the sound wave when within the min-max frequenices" do
-      music_filter = Music_Filter.new
       sound_wave = music_filter.filter([40,100,1000],40,1000)
+      expect(sound_wave).to eq [40,100,1000] 
+    end
+
+    it "should modify the sound wave with the min frequancy is not met" do 
+      sound_wave = music_filter.filter([10,100,1000],40,1000)
       expect(sound_wave).to eq [40,100,1000] 
     end
   end
